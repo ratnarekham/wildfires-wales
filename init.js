7de0ff51ed7fbef2fcd8cda11a3c833677e158bd
Ratna Rekha <manukondar@cardiff.ac.uk>
2025-08-09 21:04:43 +0100

    added popups

--- init.js
+++ init.js
@@ -1,3 +1,14 @@
-(function() {
-    console.log("Game is working");
-})();
+const clickableAreas = document.querySelectorAll('.clickable-area');
+clickableAreas.forEach(area => {
+    area.addEventListener('click', () => {
+        const clue = area.dataset.clue;
+        createPopup(clue);
+    });
+});
+
+function createPopup(clueText) {
+    const element = document.createElement('div');
+    element.classList.add('popup');
+    element.textContent = clueText;
+    document.body.appendChild(element);
+}
